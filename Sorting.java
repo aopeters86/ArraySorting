aa1e5975ccc130e7f2e038353d42a99ab6b386e3
aopeters86 <114446425+aopeters86@users.noreply.github.com>
2022-11-21 11:00:07 -0600

    Initial commit

---
+++ Sorting.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Sorting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		java.util.Scanner input = new java.util.Scanner(System.in);
+		System.out.print("Enter List: ");
+		int n = input.nextInt();
+		int[] List = new int[n]; // create the array 
+		int best = 0;
+		
+
+	}
+
+}
