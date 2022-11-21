0e685beeeb03810cbeeefc7feef5076988b554db
aopeters86 <aopeters86@yahoo.com>
2022-11-21 13:39:55 -0600

    updated
    copied array,
    
    got array to sort in a method. how to combine and make work?

--- Sorting.java
+++ Sorting.java
@@ -1,16 +1,68 @@
 import java.util.Scanner;
+import java.util.Arrays;
 public class Sorting {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		java.util.Scanner input = new java.util.Scanner(System.in);
-		System.out.print("Enter List: ");
-		int n = input.nextInt();
-		int[] List = new int[n]; // create the array 
-		int best = 0;
+		double[] firstList = {2, 1, 3, 5, 4};
+		double[] copiedList = new double[firstList.length];
 		
-
+		
+		//from the method? below to store array?
+		double sorted;
+		
+		//for loop to copy the input
+		for(int i = 0; i<firstList.length; i++) {
+			copiedList[i] = firstList[i];
+			System.out.println("--"+copiedList[i]);
+		}
+				
+		//for loop to print out list not necessary 
+		for(int i = 0; i <firstList.length ; i++) {
+			//System.out.println(firstList[i] + " ");
+			//Arrays.sort(firstList);
+			System.out.println(firstList[i] + " ");
+		
+			
+		}
+		
+		if(firstList.equals(copiedList)) {
+			System.out.println("These are sorted");
+		}
+		
+		//copy array 
+		
+		
+			
+		}
+		
+		public static String getSorted(double[] first) {
+			Arrays.sort(first);{
+				System.out.println();
+			return " ";
+			
+			
+			
+			
+		}
+		
+		
+		}
+		
 	}
 
-}
+			
+		
+
+		
+		
+		
+
+
+
+//		firstList[0] = 2;
+//		firstList[1] = 1; 
+//		firstList[2] = 3;
+//		firstList[3] = 5;
+//		firstList[4] = 4;
