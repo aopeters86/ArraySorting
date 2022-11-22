d2ef34ba607aeb36857896b65952b734364e9f1c
aopeters86 <aopeters86@yahoo.com>
2022-11-22 10:29:48 -0600

    Update SortingWithInput.java
    kinda works?

--- SortingWithInput.java
+++ SortingWithInput.java
@@ -7,7 +7,7 @@ public class SortingWithInput {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		ArrayList<Integer> firstList = new ArrayList<>();
+		ArrayList<Integer> list = new ArrayList<>();
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter Something: ");
 		
@@ -15,14 +15,14 @@ public class SortingWithInput {
 		
 		do {
 			value = input.nextInt(); //read a value from the input
-				firstList.add(value);//add value if it not in the list
+				list.add(value);//add value if it not in the list
 		}
 			while(value !=0);
 			
-			for(int i = 0; i<firstList.size(); i++)
-				System.out.print(firstList.get(i) + " ");
+			for(int i = 0; i<list.size(); i++)
+				System.out.print(list.get(i) + " ");
 		
-		//int[] firstList = {1, 2, 3, 4, 5};
+		int[] firstList = {};
 		//these print out first entered list
 		//System.out.println(Arrays.toString(firstList));
 
@@ -35,7 +35,7 @@ public class SortingWithInput {
 			if(Arrays.equals(firstList, copiedList)) {
 			System.out.println("These are sorted");
 		}
-			else {
+			{
 				System.out.println("These are not sorted");
 			}
 			
